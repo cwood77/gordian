43af6bffdc2f642924f422bc8d9a7b8cba18b5d2
cwood77 <dumuzi77@gmail.com>
2022-02-14 22:49:27 +0000

    dorking with gordian main

--- src/gordian/main.cpp
+++ src/gordian/main.cpp
@@ -1,22 +1,36 @@
-#include <exception>
-#include <stdio.h>
-#define WIN32_LEAN_AND_MEAN
-#include <windows.h>
-#include "../http/api.hpp"
+#include "../cmn/autoPtr.hpp"
+#include "../console/arg.hpp"
+#include "../console/log.hpp"
 #include "../tcatlib/api.hpp"
+#include <exception>
+#include <stddef.h>
+
+class initCommand {};
+
+class scrubCommand {
+public:
+   scrubCommand() : yes(false) {}
+   bool yes;
+};
 
 int main(int , char *[])
 {
-   printf("chris says hi\r\n");
+   using namespace console;
+   cStdOutLogSink logSink;
+   tcat::typePtr<iLogFactory> pLogFactory;
+   cmn::autoReleasePtr<iLog> pLog(&pLogFactory->createLog(logSink));
 
    try
    {
-      tcat::typePtr<http::iHttpReader> pReader;
-      pReader->read();
+      verb<initCommand> init("--init");
+      verb<scrubCommand> scrub("--scrub");
+      scrub
+         .addOption(*new boolOption("-y",offsetof(scrubCommand,yes)))
+         .addTag("--yes");
    }
    catch(std::exception& x)
    {
-      printf("error: %s\n",x.what());
+      pLog->writeLn("ERROR: %s",x.what());
    }
 
    return 0;
