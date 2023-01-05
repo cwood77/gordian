ab4695a8d0fac98f0100fa0d50def9a6178a253d
cwood77 <dumuzi77@gmail.com>
2023-01-05 02:17:49 +0000

    put teeth in uninstall

--- scriptlib/xcopy-deploy.bat
+++ scriptlib/xcopy-deploy.bat
@@ -20,6 +20,13 @@ echo %gERROR% got some sort of error from xcopy
 goto end
 
 :uninstall
+rem /S recursive
+rem /Q don't ask
+rmdir /S /Q "%gTARGET_PATH%"
+if %ERRORLEVEL% == 0 goto win
+echo errorlevel = %ERRORLEVEL%
+echo %gERROR% got some sort of error from rmdir
+goto end
 
 :win
 echo %gSUCCESS%
