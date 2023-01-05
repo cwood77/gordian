42ee3d4497f0629c90ce626c5fb7a28af7b3ebdd
cwood77 <dumuzi77@gmail.com>
2023-01-05 02:54:18 +0000

    working towards 2 gordian versions

--- make-package.bat
+++ make-package.bat
@@ -1,32 +1,46 @@
 @echo off
 setlocal
 
+if "%1" == "_subfunc" goto %1
+
 rmdir /S /Q bin\out\pack
 mkdir bin\out\pack
-mkdir bin\out\pack\gordian-8-win32-rel
 mkdir testdata\packages
 
-copy bin\out\debug\archive.dll bin\out\pack\gordian-8-win32-rel\.
-copy bin\out\debug\console.dll bin\out\pack\gordian-8-win32-rel\.
-copy bin\out\debug\curator.dll bin\out\pack\gordian-8-win32-rel\.
-copy bin\out\debug\exec.dll bin\out\pack\gordian-8-win32-rel\.
-copy bin\out\debug\file.dll bin\out\pack\gordian-8-win32-rel\.
-copy bin\out\debug\gordian.exe bin\out\pack\gordian-8-win32-rel\.
-copy bin\out\debug\http.dll bin\out\pack\gordian-8-win32-rel\.
-copy bin\out\debug\store.dll bin\out\pack\gordian-8-win32-rel\.
-copy bin\out\debug\tcatbin.dll bin\out\pack\gordian-8-win32-rel\.
-copy bin\out\debug\xcopy-deploy.bat bin\out\pack\gordian-8-win32-rel\.
+rem call myself to build a certain version
+call %0 _subfunc 8
+call %0 _subfunc 9
 
-echo # vi: ft=json>bin\out\pack\gordian-8-win32-rel.sst
-echo {>>bin\out\pack\gordian-8-win32-rel.sst
-echo    "name": "gordian",>>bin\out\pack\gordian-8-win32-rel.sst
-echo    "version": 8,>>bin\out\pack\gordian-8-win32-rel.sst
-echo    "serialize-upgrade": true,>>bin\out\pack\gordian-8-win32-rel.sst
-echo    "defer-uninstall": true>>bin\out\pack\gordian-8-win32-rel.sst
-echo }>>bin\out\pack\gordian-8-win32-rel.sst
+goto end
 
-bin\out\debug\gordian --pack bin\out\pack\gordian-8-win32-rel
+:_subfunc
+echo "=== building gordian version %2==="
 
-copy bin\out\pack\gordian-8-win32-rel.ar.z.s testdata\packages\.
-copy bin\out\pack\gordian-8-win32-rel.sst testdata\manifests\.
-rmdir /S /Q testdata\packages\gordian-8-win32-rel
+mkdir bin\out\pack\gordian-%2-win32-rel
+
+copy bin\out\debug\archive.dll bin\out\pack\gordian-%2-win32-rel\.
+copy bin\out\debug\console.dll bin\out\pack\gordian-%2-win32-rel\.
+copy bin\out\debug\curator.dll bin\out\pack\gordian-%2-win32-rel\.
+copy bin\out\debug\exec.dll bin\out\pack\gordian-%2-win32-rel\.
+copy bin\out\debug\file.dll bin\out\pack\gordian-%2-win32-rel\.
+copy bin\out\debug\gordian.exe bin\out\pack\gordian-%2-win32-rel\.
+copy bin\out\debug\http.dll bin\out\pack\gordian-%2-win32-rel\.
+copy bin\out\debug\store.dll bin\out\pack\gordian-%2-win32-rel\.
+copy bin\out\debug\tcatbin.dll bin\out\pack\gordian-%2-win32-rel\.
+copy bin\out\debug\xcopy-deploy.bat bin\out\pack\gordian-%2-win32-rel\.
+
+echo # vi: ft=json>bin\out\pack\gordian-%2-win32-rel.sst
+echo {>>bin\out\pack\gordian-%2-win32-rel.sst
+echo    "name": "gordian",>>bin\out\pack\gordian-%2-win32-rel.sst
+echo    "version": %2,>>bin\out\pack\gordian-%2-win32-rel.sst
+echo    "serialize-upgrade": true,>>bin\out\pack\gordian-%2-win32-rel.sst
+echo    "defer-uninstall": true>>bin\out\pack\gordian-%2-win32-rel.sst
+echo }>>bin\out\pack\gordian-%2-win32-rel.sst
+
+bin\out\debug\gordian --pack bin\out\pack\gordian-%2-win32-rel
+
+copy bin\out\pack\gordian-%2-win32-rel.ar.z.s testdata\packages\.
+copy bin\out\pack\gordian-%2-win32-rel.sst testdata\manifests\.
+rmdir /S /Q testdata\packages\gordian-%2-win32-rel
+
+:end
